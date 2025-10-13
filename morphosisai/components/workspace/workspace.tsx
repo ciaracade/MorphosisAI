0d716844d702ac0d7904d6b53b26db5b708ce13f
cadeci <ciaracade03@gmail.com>
2025-10-13 18:33:10 -0400

    wip: Implement base control

--- morphosisai/components/workspace/workspace.tsx
+++ morphosisai/components/workspace/workspace.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Panel, ReactFlow, Background, Controls, type ColorMode, MiniMap } from "@xyflow/react";
+import { Panel, ReactFlow,  ControlButton, Background, Controls, type ColorMode, MiniMap } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { useState } from 'react';
 import { ModeToggle } from './canvas/theme-toggle';
+import { MagicWandIcon } from "@radix-ui/react-icons";
 
 export default function Workspace() {
   const [colorMode, setColorMode] = useState<ColorMode>("dark");
@@ -12,7 +13,13 @@ export default function Workspace() {
     <div style={{ height: "100%", width: "100%" }}>
       <ReactFlow colorMode={colorMode}>
         <Background />
-        <Controls />
+        <Controls position="top-center" orientation="horizontal">
+          <ControlButton
+            onClick={() => alert("Something magical just happened. ✨")}
+          >
+            <MagicWandIcon />
+          </ControlButton>
+        </Controls>
         <MiniMap pannable zoomable />
         <Panel position="top-right">
           <ModeToggle onThemeChange={setColorMode} />
